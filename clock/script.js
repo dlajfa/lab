1bbba6e4ac33b44832428e0046b05bdf66c98462
dlajfa <151374735+dlajfa@users.noreply.github.com>
2024-01-09 09:58:43 +0800

    Update script.js

--- clock/script.js
+++ clock/script.js
@@ -2,19 +2,27 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
+const dateEl = document.getElementById('date');
+
 showTime();
 setInterval(showTime, 1000);
 
 function showTime() {
-  const time = new Date();
+  const now = new Date();
 
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
 
   hoursEl.innerText = format(hours);
   minutesEl.innerText = format(minutes);
   secondsEl.innerText = format(seconds);
+
+  dateEl.innerText = `${year}-${format(month)}-${format(day)}`;
 }
 
 function format(count) {
